4c1a4b1b210eb50c3e972097f8b1be697fae54c7
Alex Galimzyanov <alexofficialkek@gmail.com>
2023-04-19 15:49:54 +0500

    LMS_HOST

--- urfu/interactive.py
+++ urfu/interactive.py
@@ -18,7 +18,7 @@ def ask_questions(config: Config, run_for_prod: Optional[bool] = None) -> None:
     """
     defaults = urfu_config.get_defaults()
     if run_for_prod is None:
-        run_for_prod = config.get("LMS_HOST") != "local.overhang.io"
+        run_for_prod = config.get("LMS_HOST") != "local.urfu.online.ru"
         run_for_prod = click.confirm(
             fmt.question(
                 "Are you configuring a production platform? "
@@ -29,8 +29,8 @@ def ask_questions(config: Config, run_for_prod: Optional[bool] = None) -> None:
         )
     if not run_for_prod:
         dev_values: Config = {
-            "LMS_HOST": "local.overhang.io",
-            "CMS_HOST": "studio.local.overhang.io",
+            "LMS_HOST": "local.urfu.online.ru",
+            "CMS_HOST": "studio.local.urfu.online.ru",
             "ENABLE_HTTPS": False,
         }
         fmt.echo_info(
